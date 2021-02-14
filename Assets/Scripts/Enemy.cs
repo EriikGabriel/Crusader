b3ef11490a487b138ab283ccb391801bc8a2e028
EriikGabriel <erikgabriel.lins@hotmail.com>
2021-02-14 17:47:52 -0300

    Commit - Arquivos de Base do Jogo

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Speed;
+  
+    public float stopingDistance;
+    private Transform Target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Está pegando o game object do palayer e depois adicionando o Transform.
+        Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+        void Update()
+        {
+            if(!Playervida.isDestroy)
+            {
+                 // Só  vai  movimentar o  inimigo  se a distancia for menor que a distância definida.
+                if(Vector2.Distance(transform.position, Target.position) <  stopingDistance)
+                {
+                    transform.position =  Vector2.MoveTowards(transform.position, Target.position, Speed * Time.deltaTime);
+                }
+            }
+           
+        }
+}
---
+++ Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float speed = 0f;
+    public float shotDistance = 3f;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+    public GameObject shot;
+
+    Vector2 moviment;
+    Vector2 mousePos;
+
+    // Fixed Update is called once per frame
+    void FixedUpdate()
+    {
+        moviment.x = Input.GetAxisRaw("Horizontal");
+        moviment.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", moviment.x);
+        anim.SetFloat("Vertical", moviment.y);
+        anim.SetFloat("Speed", moviment.magnitude);
+
+        rb.MovePosition(rb.position + moviment.normalized * speed * Time.fixedDeltaTime);
+
+        if (Input.GetButtonDown("Fire1")) {
+            anim.SetTrigger("Shot");
+        }
+
+        if(moviment != Vector2.zero)
+        {
+            anim.SetFloat("Idle_Horizontal", moviment.x);
+            anim.SetFloat("Idle_Vertical", moviment.y);
+        }
+     }
+}
